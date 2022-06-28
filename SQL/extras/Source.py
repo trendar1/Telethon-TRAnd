09bedc07fa951f0377e73d18cdb62dc612cedd47
trendar1 <108071920+trendar1@users.noreply.github.com>
2022-06-28 22:36:07 +0300

    Add files via upload

---
+++ SQL/extras/Source.py
@@ -0,0 +1,11 @@
+import re
+import time
+from datetime import datetime
+from userbot import StartTime, iqthon
+from userbot.Config import Config
+from userbot.plugins import mention
+help1 = ("**᥀︙ كيفيه التنصيب :**")
+help2 = ("**᥀︙ قـائمـه الاوامـر :**\n**᥀︙ قنـاه السـورس :** @VV399\n**᥀︙ شـرح اوامـر السـورس : @VV190**\n**᥀︙ شـرح فـارات السـورس : @VVMAT** \n - اوامر الاونلاين تشتغل فقط في المجموعات ")
+TG_BOT = Config.TG_BOT_USERNAME
+TM = time.strftime("%I:%M")
+Sour = f"**↯ : version 7.6  𓇡.** \n↯: me  {mention}  𓇡. \n**↯: time  {TM}  𓇡.**\n**↯: My Bot {TG_BOT} 𓇡.**\n**↯: Source : @VV399  𓇡.**"
