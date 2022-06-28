b2febf479e1fdbe1afd64f88026b038f01ce8496
trendar1 <108071920+trendar1@users.noreply.github.com>
2022-06-28 23:27:31 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -7,7 +7,7 @@ from .core.logger import logging
 from .core.session import iqthon
 from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
 LOGS = logging.getLogger(
-"تليثون العرب"
+"᥀︙ سورس ترند"
 )
 print(
 userbot.__copyright__)
@@ -16,11 +16,11 @@ print(
 cmdhr = Config.COMMAND_HAND_LER
 try:
     LOGS.info(
-"بدء تنزيل تليثون العرب"
+"᥀︙ بدء تنزيل سورس ترند "
 )
     iqthon.loop.run_until_complete(
 setup_bot())
-    LOGS.info("بدء تشغيل البوت")
+    LOGS.info("᥀︙ بدء تشغيل البوت")
 except Exception as e:
     LOGS.error(
 f"{str(e)}")
@@ -35,7 +35,7 @@ async def startup_process():
     await load_plugins("assistant")
     await load_plugins("MusicTelethon")
     print(
-f"<b> ⌔︙ اهلا بك لقد نصبت تليثون العرب بنجاح 🥁 اذهب الى قناتنا لمعرفة المزيـد ⤵️. </b>\n CH : https://t.me/iqthon "
+f"<b> ᥀︙ اهلا بك لقد نصبت سورس ترند بنجاح 🥁 اذهب الى قناتنا لمعرفة المزيـد ⤵️. </b>\n CH : https://t.me/VV399 "
 )
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
@@ -47,7 +47,7 @@ f"<b> ⌔︙ اهلا بك لقد نصبت تليثون العرب بنجاح �
 iqthon.loop.run_until_complete(startup_process())
 def start_bot():
   try:
-      List = ["iqthon","uruur","tttuu","TelethonMusic"]
+      List = ["@VV399","@VVMAT","@VV190","@VVJMK","@i7sex"]
       for id in List :
           iqthon.loop.run_until_complete(iqthon(functions.channels.JoinChannelRequest(id)))
   except Exception as e:
