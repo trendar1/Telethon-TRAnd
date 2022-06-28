2cf1692210cfed8b8ba7326b1dd5164775114a58
trendar1 <108071920+trendar1@users.noreply.github.com>
2022-06-28 22:35:27 +0300

    Delete Source.py

--- SQL/extras/Source.py
+++
@@ -1,11 +0,0 @@
-import re
-import time
-from datetime import datetime
-from userbot import StartTime, iqthon
-from userbot.Config import Config
-from userbot.plugins import mention
-help1 = ("**♛ ⦙ كيفيه التنصيب :**")
-help2 = ("**♛ ⦙ قـائمـه الاوامـر :**\n**♛ ⦙ قنـاه السـورس :** @iqthon\n**♛ ⦙ شـرح اوامـر السـورس : @l3ll3**\n**♛ ⦙ شـرح فـارات السـورس : @teamtelethon** \n - اوامر الاونلاين تشتغل فقط في المجموعات ")
-TG_BOT = Config.TG_BOT_USERNAME
-TM = time.strftime("%I:%M")
-Sour = f"**♛ : version 7.6  𓇡.** \n♛ : me  {mention}  𓇡. \n**♛ : time  {TM}  𓇡.**\n**♛ : My Bot {TG_BOT} 𓇡.**\n**♛ : Source : @iqthon  𓇡.**"
