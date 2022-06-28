5eedd9769fad83612d8ae4618091d4e1d8f0c460
trendar1 <108071920+trendar1@users.noreply.github.com>
2022-06-28 22:43:24 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -18,7 +18,7 @@ class Config(object):
     TG_BOT_USERNAME = None
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "https://github.com/telethonArab/telethonAr")
-    AUTONAME = os.environ.get("AUTONAME", "@iqthon")
+    AUTONAME = os.environ.get("AUTONAME", "@VV399")
     PRIVATE_GROUP_BOT_API_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID") or 0)
     PRIVATE_GROUP_ID = int(os.environ.get("PRIVATE_GROUP_ID") or 0)
     PRIVATE_CHANNEL_BOT_API_ID = int(os.environ.get("PRIVATE_CHANNEL_BOT_API_ID") or 0)
